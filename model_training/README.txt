0ce1c0027bb50571f550f94611e9fa713d277977
Taehee Jung <taehee.jung.1224@gmail.com>
2025-02-26 15:17:59 -0600

    initial push for data and model training code

---
+++ model_training/README.txt
@@ -0,0 +1,19 @@
+Code is for fine-tuning Alpaca and Llama2 from https://github.com/tatsu-lab/stanford_alpaca
+
+1. Go to stanford_alpaca directory
+2. Do: pip install -r requirements.txt 
+3. pip install deepspeed
+4. pip install accelerator
+5. Run model_trainer.sh
+
+Note:
+a. In model_trainer.sh
+		If you want to use alpaca: 
+			--model_name_or_path "wxjiao/alpaca-7b"		
+
+b. Don't change other setups except for
+		i. --data_path ../../data/train/coi_2.json \
+			this is where the train data is located (change the path if needed)
+
+		ii. --output_dir ../../alpaca_models/coi2\
+			this is where the model is stored (change the path if needed)
---
+++ model_training/stanford_alpaca/model_trainer.sh
@@ -0,0 +1,19 @@
+torchrun --nproc_per_node=8 --master_port=5553 train.py \
+    --model_name_or_path "wxjiao/alpaca-7b" \
+     --data_path ../../data/train/coi_2.json \
+    --bf16 True \
+    --output_dir ../../alpaca_models/coi2 \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --deepspeed "./configs/default_offload_opt_param.json" \
+    --tf32 True \
+    --report_to "none"
